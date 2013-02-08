3ecc0d82710ccc9f308d8ab99ce1b753249fbb22
Sriram Damodharan <ultimatevikrant@gmail.com>
2013-02-08 18:30:51 +0530

    Removed unused imports
    Removed unused imports in MainActivity.java

--- MobileApp/WifiConnection/src/org/sas04225/wificonnection/MainActivity.java
+++ MobileApp/WifiConnection/src/org/sas04225/wificonnection/MainActivity.java
@@ -4,25 +4,21 @@ package org.sas04225.wificonnection;
 
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.util.List;
-import org.sas04225.wificonnection.R;
-
-
-import android.os.Bundle;
-import android.os.Environment;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.net.wifi.*;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 
