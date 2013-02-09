56f574bc2aa21d6a4262909b45d263cfe7535db2
Sriram Damodharan <ultimatevikrant@gmail.com>
2013-02-09 11:49:50 +0530

    Added Netbeans project stubs
    Added Netbeans project stubs for DescriptorSetBuilder Frontend and Backend

---
+++ ServerSideApp/DescriptorSetBuilderBackend/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: sriram
+ *
+ * Created on February 9, 2013, 11:08 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ ServerSideApp/DescriptorSetBuilderFrontend/src/org/sas04225/DescriptorSetBuilder/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sas04225.DescriptorSetBuilder;
+
+/**
+ *
+ * @author sriram
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public final String cache_dir = "Development/DescriptorSetCache";
+    public final String default_repo_dir = "Development/Databases/camera-repo";
+    
+    public static void main(String[] args) {
+        
+    }
+}
