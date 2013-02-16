2e0add74baa4512574ee836d9a7c0e953949e072
Archana Rajan <archie.rajan@gmail.com>
2013-02-16 20:04:49 +0530

    new popup

--- MobileApp/PopupAlert/src/com/example/popupalert/Pop_Tart.java
+++ MobileApp/PopupAlert/src/com/example/popupalert/Pop_Tart.java
@@ -1,33 +1,33 @@
-package org.sas04225.wificonnection;
+package com.example.popupalert;
 
-import java.util.List;
+
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
-import android.net.wifi.ScanResult;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.widget.Toast;
+
+@SuppressLint("NewApi") public class Pop_Tart extends DialogFragment {
 
 
-@SuppressLint("NewApi") public class AlertPopupDialogue extends DialogFragment{
+
 	 public static interface NoticeDialogListener {
 	        public void onDialogPositiveClick(DialogFragment dialog);
 	        public void onDialogNegativeClick(DialogFragment dialog);
 	    }
 	 NoticeDialogListener mListener;
-	 @Override
+	 @TargetApi(Build.VERSION_CODES.HONEYCOMB) @SuppressLint("NewApi") @Override
 	    public Dialog onCreateDialog(Bundle savedInstanceState) {
 		 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		 builder.setTitle(R.string.alert_title);
+		 builder.setTitle(R.string.title);
 		 LayoutInflater inflater = getActivity().getLayoutInflater();
-		 builder.setView(inflater.inflate(R.layout.alert_popup_dialogue,null)).setPositiveButton(R.string.ok_but,new DialogInterface.OnClickListener() {
+		 builder.setView(inflater.inflate(R.layout.pop,null)).setPositiveButton(R.string.ok_but,new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int id) {
 				// TODO Auto-generated method stub
@@ -35,10 +35,10 @@ import android.widget.Toast;
 			}
 		})
 		 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   getDialog().cancel();
-               }
-           });
+              @TargetApi(Build.VERSION_CODES.HONEYCOMB) public void onClick(DialogInterface dialog, int id) {
+                  getDialog().cancel();
+              }
+          });
 		 return null;
 	 }
 		 @Override
