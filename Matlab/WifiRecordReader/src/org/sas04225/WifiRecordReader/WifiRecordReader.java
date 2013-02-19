d0099fe988feae389b274d151d790a53062df439
Sriram Damodharan <ultimatevikrant@gmail.com>
2013-02-19 11:38:07 +0530

    WifiRecordReader

---
+++ Matlab/WifiRecordReader/src/org/sas04225/WifiRecordReader/WifiRecordReader.java
@@ -0,0 +1,54 @@
+package org.sas04225.WifiRecordReader;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import org.sas04225.proto.WifiScanResultProto.WifiScanResult;
+
+public class WifiRecordReader {
+
+    private File filePath;
+    InputStream in;
+
+    public WifiRecordReader(String filename) throws FileNotFoundException {
+        filePath = new File(filename);
+        in = new FileInputStream(filePath);
+    }
+
+    public WifiScanResult nextResult() {
+        WifiScanResult result = null;
+        try {
+            if (in.available() == 0) {
+                return null;
+            }
+            byte[] data = readMessageData(in);
+            try {
+                result = WifiScanResult.parseFrom(data);
+            } catch (InvalidProtocolBufferException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static byte readMessageData   (InputStream in)  [] throws IOException {
+        int len = 0;
+
+        len = in.read();
+        len |= (in.read()) << 8;
+        len |= (in.read()) << 16;
+        len |= (in.read()) << 24;
+
+        byte[] data = new byte[len];
+        in.read(data, 0, len);
+
+        return data;
+    }
+}
